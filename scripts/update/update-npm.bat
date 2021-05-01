f877a261504005bef6f8682e673e852331182699
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-05-01 17:30:03 +0200

    Update update-npm.bat

--- scripts/update/update-npm.bat
+++ scripts/update/update-npm.bat
@@ -1,3 +1,9 @@
 @ECHO off
 ECHO Update all dependencies...
-FOR /D %%a IN (%CD%\fosscord\*) do echo -------------- && echo %%~fa && cd %%~fa && npm i
+where /q pnpm
+IF ERRORLEVEL 1 (
+	FOR /D %%a IN (%CD%\fosscord\*) do echo -------------- && echo %%~fa && cd %%~fa && npm i
+) ELSE (
+  FOR /D %%a IN (%CD%\fosscord\*) do echo -------------- && echo %%~fa && cd %%~fa && pnpm i
+)
+@ECHO on
